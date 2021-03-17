4adc286e65b64272bf78ac361f892eadf8283c95
Oscar Nevarez <fu.wire@gmail.com>
2021-03-16 19:30:16 -0600

    Clean up execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,9 @@ set -o pipefail
 echo "Arg0?:= $1"
 echo "directory?:= $2"
 
-readonly task_directory="$2"
-
 time=$(date)
 echo "::set-output name=time::$time"
 
-pushd $task_directory
+#readonly task_directory="$2"
+#pushd $task_directory
 $BUILDER_HOME/scripts/main.sh
-popd > /dev
