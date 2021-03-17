4c7c7d3a5b2ce8201e36899895d199e7ece2f928
Oscar Nevarez <fu.wire@gmail.com>
2021-03-17 10:42:50 -0600

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,19 +9,28 @@ time=$(date)
 echo "::set-output name=time::$time"
 
 readonly task_directory="$2"
-pushd $task_directory
+readonly WORDLY_PLACE=$(mktemp -d)
 
+# 1)
+# Step name: input-gather
+# Purpose: Get the settings from task.yml
+# Path: TaskDirectory
+pushd $task_directory
 readonly json_task=$(yq eval -j -I=0 ./task.yml)
 readonly pkg_name=$(echo $json_task | jq -rc '.name')
 readonly pkg_version=$(echo $json_task | jq -rc '.version')
-
 #grab action run
 readonly run_content=$(echo $json_task | jq -rc '.run')
-readonly script_file=./bin/user_build_script
+readonly script_file=./bin/user_script
 
 mkdir -p bin
-mkdir -p dist
-mkdir -p tmp
+mkdir -p dist/task/bin
+
+
+# 2)
+# Step name: buildpack-config-generation
+# Purpose: Create buildpack.toml and package.toml
+# Path: TaskDirectory
 
 # Create package.toml
 cat <<EOF >./package.toml
@@ -51,6 +60,11 @@ homepage = "https://laraboot.io/tasks/user/$pkg_name"
 id = "io.buildpacks.stacks.bionic"
 EOF
 
+# 3)
+# Step name: buildpack-user-script
+# Purpose: Put the custom script on a file to be executed by lifecycle
+# Path: TaskDirectory
+
 cat <<EOF >$script_file
 #!/usr/bin/env bash
 set -eu
@@ -63,11 +77,24 @@ chmod +x $script_file
 
 popd
 
+# 3)
+# Step name: build-time
+# Purpose: compile bin and detect binaries using user data
+# Path: Builder home (where GO source lives)
+pushd $BUILDER_HOME
 GOOS=linux go build -ldflags "-X 'main.TaskName=$pkg_name' -s -w" -o ./bin/detect ./cmd/detect/main.go &&
-  GOOS=linux go build -ldflags "-s -w" -o ./bin/build ./cmd/build/main.go &&
-  chmod -R +x ./bin
+  GOOS=linux go build -ldflags "-s -w" -o ./bin/build ./cmd/build/main.go
+popd
 
+pushd $task_directory
+cp -r $BUILDER_HOME/bin/* ./bin
+chmod -R +x ./bin
 pack buildpack package my-task --config package.toml
+popd
+
+#cp -r $WORDLY_PLACE/*.toml $BUILDER_WORKBENCH/dist/task &&
+#cp -r $WORDLY_PLACE/bin/* $BUILDER_WORKBENCH/dist/task/bin &&
+#cp -r $GO_PROJECT_DIR/bin/* $BUILDER_WORKBENCH/dist/task/bin
 
 #pwd
 #ls -ltah
