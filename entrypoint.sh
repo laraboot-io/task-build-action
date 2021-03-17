78853df7f03ef5113a6943be66843a020f7ce7fa
Oscar Nevarez <fu.wire@gmail.com>
2021-03-17 00:23:55 -0600

    Add directory input

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ echo "args = $*"
 time=$(date)
 echo "::set-output name=time::$time"
 
-#readonly task_directory="$2"
-#pushd $task_directory
+readonly task_directory="$2"
+pushd $task_directory
+pwd
+ls -ltah
 $BUILDER_HOME/scripts/main.sh
+popd
