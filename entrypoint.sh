2b62d13fc9c17f800f506617be44df78c24b8c20
Oscar Nevarez <fu.wire@gmail.com>
2021-03-19 15:12:58 -0600

    buildpack.toml

--- entrypoint.sh
+++ entrypoint.sh
@@ -109,6 +109,7 @@ popd
 pushd $task_directory > /dev/null
 cp -r $BUILDER_HOME/bin/* ./bin
 chmod -R +x ./bin
+cat buildpack.toml
 cat package.toml
 pack buildpack package my-task --config package.toml
 popd
